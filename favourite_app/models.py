24bb065c4af9d3e5cfb58b92e7650351a2c5c664
Samuel Schaefer <samuelschaefer@Samuels-MacBook-Pro.local>
2019-04-16 12:07:47 -0400

    last time with models, I hope

--- favourite_app/models.py
+++ favourite_app/models.py
@@ -19,12 +19,16 @@ class Guru(models.Model):
     image_url = models.CharField(max_length=400, blank=True)
     skill_set = models.CharField(max_length=80)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name='suppliers')
     guru = models.ForeignKey(Guru, on_delete=models.CASCADE, related_name='Gurus')
     name: models.CharField(max_length=75)
     picture_url = models.CharField(max_length=400)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
