b81cfad2f9808695342d985b3a529263bd69093e
Samuel Schaefer <samuelschaefer@Samuels-MacBook-Pro.local>
2019-05-07 16:39:23 -0400

    extensions added for debugging heroku issue

--- final_project/settings.py
+++ final_project/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'favourite_app'
+    'favourite_app',
+    'django-extensions',
 ]
 
 MIDDLEWARE = [
